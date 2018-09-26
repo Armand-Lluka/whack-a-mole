bc9adef834fe2fb5e5f41bc31a38d27960f33ac8
zeanort <armandwork@hotmail.co.uk>
2018-09-27 00:08:42 +0100

    hammer display

--- src/components/grid/grid.js
+++ src/components/grid/grid.js
@@ -1,22 +1,23 @@
 import React from 'react'
+import hammer from '../../../public/icons/hammer.svg'
 
 class Grid extends React.Component {
 
     render(){
         return (
-            <section class="grid" id="">
+            
             <div class="grid-container">
-                <div id="box-1">1</div>
-                <div id="box-2">2</div>
-                <div id="box-3">3</div>
-                <div id="box-4">4</div>
-                <div id="box-5">5</div>
+                <div class="mole" id="box-1">1</div>
+                <div class="mole" id="box-2">2</div>
+                <div class="mole" id="box-3">3</div>
+                <div class="mole" id="box-4">4</div>
+                <div class="mole" id="box-5">5</div>
                 <div id="box-6">6</div>
                 <div id="box-7">7</div>
                 <div id="box-8">8</div>
                 <div id="box-9">9</div>
             </div>
-            </section>
+          
         )
     }
 }
