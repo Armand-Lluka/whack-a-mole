2592932cfe18f883e8346a1427bcc71328a563d4
harryyy27 <harryyy27@gmail.com>
2018-09-28 10:25:07 +0100

    moles

--- src/components/additional-components/additional-components.js
+++ src/components/additional-components/additional-components.js
@@ -1,11 +1,14 @@
 import React from 'react';
-// import Grid from '../grid/grid.js'
+import Grid from '../grid/grid.js'
 
 class Timers extends React.Component {
-  state = {
+  
+    state = {
       running: false,
       time: 120,
     }
+  
+  
   getSeconds = () => {
       return ('0'+ this.state.time%60).slice(-2);        
   }
@@ -17,9 +20,13 @@ class Timers extends React.Component {
       this.setState(prevState => {
         const _this = this;
           const startTime = Date.now() - prevState.time;
+          this.child.moleTimer();
           this.timer = setInterval(() => {
               if(_this.state.time>0){
             this.setState({ time: Math.ceil(120 - (Date.now() - startTime)/1000) });
+          }
+          else {
+            clearInterval(this.child.moleTimer);
           }
           }, 1000);
         
@@ -36,7 +43,7 @@ class Timers extends React.Component {
           <button onClick={this.timer}>
             WHACK SOME MOLES
           </button>
-          {/* <Grid /> */}
+          <Grid ref={instance => { this.child = instance; }}/>
           
         </div>
       );
